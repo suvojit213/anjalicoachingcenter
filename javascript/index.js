f026edadb6be0a38c039859aac1b12a1410cf937
Suvo Developer <107928380+suvojit213@users.noreply.github.com>
2023-12-08 11:39:22 +0530

    Add files via upload

--- javascript/index.js
+++
@@ -1,6 +0,0 @@
-const sidenav = document.querySelector('.sidenav');
-const hideBtn = document.querySelector('.sidenav-hide-btn');
-
-hideBtn.addEventListener('click', () => {
-  sidenav.classList.toggle('hidden');
-});
